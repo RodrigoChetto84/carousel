d6950b70e786e0fb39f6ebfa80ecf225344101c7
coton_ete <coton_ete@MacBook-Pro-de-coton-ete.local>
2017-07-18 16:49:42 -0300

    Initial Commit

---
+++ CarrouselTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarrouselTest
+//
+//  Created by coton_ete on 18/7/17.
+//  Copyright © 2017 RodrigoChetto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
