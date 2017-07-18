a826b761dd214c214b0afd9143367438b9b10bfa
coton_ete <coton_ete@MacBook-Pro-de-coton-ete.local>
2017-07-18 19:12:37 -0300

    changes to maincontroller and project

--- CarrouselTest/ViewController.swift
+++ CarrouselTest/ViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    
+    
+    let carrouselJson = [
+        [
+            "title": "Carousel thumb",
+            "type": "thumb",
+            "items": [
+                "title": "La Playa",
+                "url": "",
+                "video": ""
+            ]
+        ]
+    ];
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return carrouselJson.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let carousel = tableView.dequeueReusableCell(withIdentifier: "carouselCell") as! iCarousel
+        return carousel as! UITableViewCell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +45,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
